0735b22b11c215b325c1f2997777b0528dea7d57
jGalvann <joaogalvan02@gmail.com>
2024-10-16 13:57:56 -0300

    lembra de não criar um arquivo readme direto na pasta do repositório aparentemente corrempe a pasta ou algo assim

---
+++ Atividades Segundo Bimestre/att01-gpt/src/gpt01.java
@@ -0,0 +1,17 @@
+// soma de três números
+
+/*
+ código dado pelo chatGPT
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int c = 15;
+        int resultado = a + b + c;
+        System.out.println("O resultado da soma é: " + resultado);
+    }
+}
+ */
+
+public class gpt01 {
+}
---
+++ Atividades Segundo Bimestre/att01-gpt/src/gpt05.java
@@ -0,0 +1,26 @@
+// Verificação de número primo
+
+/*
+
+public class Main {
+    public static void main(String[] args) {
+        int numero = 17;
+        boolean primo = true;
+
+        for (int i = 2; i <= numero / 2; i++) {
+            if (numero % i == 0) {
+                primo = false;
+                break;
+            }
+        }
+
+        if (primo) {
+            System.out.println(numero + " é um número primo.");
+        } else {
+            System.out.println(numero + " não é um número primo.");
+        }
+    }
+}
+ */
+public class gpt05 {
+}
