02559244914512c7513e55c0cc6cf857a558222a
jGalvann <joaogalvan02@gmail.com>
2024-10-16 14:01:00 -0300

    sla q q ta dando de errado

---
+++ Atividades Segundo Bimestre/att01/att01-gpt/src/gpt03.java
@@ -0,0 +1,16 @@
+// Cálculo de área de um retângulo
+
+
+/*
+
+public class Main {
+    public static void main(String[] args) {
+        int altura = 5;
+        int largura = 8;
+        int area = altura * largura;
+        System.out.println("A área do retângulo é: " + area);
+    }
+}
+ */
+public class gpt03 {
+}
