627afe34061595ec644e0a76bce825a5f3f155d1
jGalvann <joaogalvan02@gmail.com>
2024-10-16 14:01:00 -0300

    Atualização e resolução das 5 att

---
+++ Atividades Segundo Bimestre/att01/att01-gpt/src/gpt04.java
@@ -0,0 +1,16 @@
+// Tabuada de um número
+
+/*
+
+public class Main {
+    public static void main(String[] args) {
+        int numero = 6;
+        for (int i = 1; i <= 10; i++) {
+            int resultado = numero * i;
+            System.out.println(numero + " x " + i + " = " + resultado);
+        }
+    }
+}
+ */
+public class gpt04 {
+}
