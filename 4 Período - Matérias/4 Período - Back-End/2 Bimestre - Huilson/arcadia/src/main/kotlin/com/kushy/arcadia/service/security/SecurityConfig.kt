b21aaeb12bfc839f623a516573bd26259cbbd4b9
jGalvann <joaogalvan02@gmail.com>
2025-11-30 19:40:30 -0300

    resolvendo uma request q tava dando erro

--- 4 Período - Matérias/4 Período - Back-End/2 Bimestre - Huilson/arcadia/src/main/kotlin/com/kushy/arcadia/service/security/SecurityConfig.kt
+++ 4 Período - Matérias/4 Período - Back-End/2 Bimestre - Huilson/arcadia/src/main/kotlin/com/kushy/arcadia/service/security/SecurityConfig.kt
@@ -37,7 +37,7 @@ class SecurityConfig( // Essa é uma classe meio que padrão do Spring, mas aind
                 authorize   // Aqui ele faz com que os endPoints abaixo não precisem de autenticação.
                     .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                     .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
-                    .requestMatchers(HttpMethod.GET, "/games").permitAll()
+                    .requestMatchers("/error").permitAll()
 
                     .anyRequest().authenticated() // Qualquer outra request, o user vai precisar estar autenticado
                                                   // Caso não esteja, vai ter um 403 Forbidden pro maluco
