1861d68a730b61913471688f3306d6bc10aa296d
jGalvann <joaogalvan02@gmail.com>
2026-03-16 16:21:45 -0300

    feat: adiciona contador simples com incremento e decremento

--- 5 Período - Matérias/5 Período - Desenvolvimento Android/truco-contador/App.js
+++ 5 Período - Matérias/5 Período - Desenvolvimento Android/truco-contador/App.js
@@ -1,11 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
 
 export default function App() {
+
+  const [pontuacao, setPontuacao] = useState(0);
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
+      <Text style={styles.titulo}> Marcador</Text>
+      <Text style={styles.numero}>{pontuacao}</Text>
+
+      <View style={styles.containerBotao}>
+        <View style={styles.botao}>
+        <Button 
+        color="#01633d"
+        title="+" 
+        onPress={() => setPontuacao(pontuacao + 1)} 
+        />  
+      </View>
+
+      <View style={styles.botao}>
+        <Button 
+        color="#87092a"
+        title="-" 
+        onPress={() => setPontuacao(pontuacao > 0 ? pontuacao - 1 : 0)} 
+        />  
+      </View>
+
+      </View>
+
+
     </View>
   );
 }
@@ -17,4 +45,26 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  titulo: {
+    fontSize: 26,
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  numero: {
+    fontSize: 100,
+    fontWeight: 'bold',
+    color: '#333',
+    marginBottom: 150,
+  },
+  containerBotao: {
+    flexDirection: 'row',
+    gap: 20,
+  },
+  botao: {
+    width: 100,
+    height: 100,
+    
+  },
+
+
 });
